8a8663851647d32d7a0c34e0cc564894d45be2f3
HenryX <hx2209@columbia.edu>
2018-04-07 18:43:12 -0400

    search functions

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,7 +1,14 @@
 const User = require('../models/user.model.js');
 
 exports.getInfo = (req, res) => {
-
+    var name = req.params.name;
+    User.findOne({name:name}, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
 };
 
 exports.getMutualFriends = (req, res) => {
@@ -9,11 +16,17 @@ exports.getMutualFriends = (req, res) => {
 };
 
 exports.isFriend = (req, res) => {
-
+    var name = req.params.name1;
+    User.findOne({name:name}, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
 };
 
 exports.create = (req, res) => {
-
 };
 
 exports.befriend = (req, res) => {
