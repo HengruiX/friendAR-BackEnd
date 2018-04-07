5d58a45e7bc6eda8fe867aec0247c6f196581604
HenryX <hx2209@columbia.edu>
2018-04-07 18:14:15 -0400

    routes

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,25 +1,25 @@
 const User = require('../models/user.model.js');
 
-exports.userinfo = (req, res) => {
+exports.getInfo = (req, res) => {
 
 };
 
-// Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
+exports.getMutualFriends = (req, res) => {
 
 };
 
-// Find a single note with a noteId
-exports.findOne = (req, res) => {
+exports.isFriend = (req, res) => {
 
 };
 
-// Update a note identified by the noteId in the request
-exports.update = (req, res) => {
+exports.create = (req, res) => {
 
 };
 
-// Delete a note with the specified noteId in the request
+exports.befriend = (req, res) => {
+
+};
+
 exports.delete = (req, res) => {
-
+    
 };
