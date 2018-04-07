29d02572d211bb74c88acbd3703e5a3386402cb5
Alice Chen <mc3917@columbia.edu>
2018-04-07 18:42:56 -0400

    create

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -13,10 +13,32 @@ exports.isFriend = (req, res) => {
 };
 
 exports.create = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+    const user = new User({
+        name: req.body.name,
+        bio: req.body.bio
+    }
+    user.save()
+
+    )
 
 };
 
 exports.befriend = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    User.findOne({"name": req.body.requester},"friends",function(err,user){
+        if (err) return handleError(err);
+        person.friends.push(req.body.requested)
+    })
 
 };
 
